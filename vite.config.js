1a0f066dbff2b6f4bd8ab55b54c66397263acb4f
Marco <macdev.gmail@gmail.com>
2022-09-03 02:05:06 -0500

    gh-pages added

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    base: '/mars-rover-photos/'
+    ,
     rollupOptions: {
       external: /^lit/
     }
